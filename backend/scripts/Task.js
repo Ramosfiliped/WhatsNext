24b0e664996283be7b2286ab658b95267ac83ebd
Carlos Freitas <carlos.gabriel@aluno.ufop.edu.br>
2022-05-11 21:24:59 -0300

    Atualizando classe Task com weekDays, timeBegin e timeEnd

--- backend/scripts/Task.js
+++ backend/scripts/Task.js
@@ -1,18 +1,42 @@
 class Task{
+    //Default values for the Task class.
     description = "";
     duration = 0;
-    dispBegin = [0,0]; //Matheus, poe a hora do jeito que achar melhor [fst, snd] => fst hora de inicio / snd minuto de inicio
-    dispEnd = [0,0]; //Matheus, poe a hora do jeito que achar melhor
+    dispBegin = [0, 0];
+    dispEnd = [0, 0];
     isDone = False;
+    weekDays = [0, 0, 0, 0, 0, 0, 0];
+    timeBegin = [0, 0];
+    timeEnd = [0, 0];
 
-    constructor(description, duration, dispBegin, dispEnd, isDone) {
-        this.description = description; //String - Description of the task
-        this.duration = duration;       //Float  - Time in minutes of the task
-        this.dispBegin = dispBegin;     //Time   - Begin of diponibility of the task
-        this.dispEnd = dispEnd;         //Time   - End of diponibility of the task
-        this.isDone = isDone;      //Boolean - True => If task completed / False => If task not completed  
+    constructor(description, duration, dispBegin, dispEnd, isDone, weekDays, timeBegin, timeEnd) {
+        //String - Description of the task
+        this.description = description; 
+
+        //Int - Time in minutes of the task
+        this.duration = duration;
+
+        //[Int, Int] - Begin of diponibility of the task in [hours, minutes]
+        this.dispBegin = dispBegin; 
+
+        //[Int, Int] - End of diponibility of the task in [hours, minutes]
+        this.dispEnd = dispEnd; 
+
+        //Boolean - True => If the task's checkbox was marked / False => Otherwise
+        this.isDone = isDone; 
+
+        //Vector with 7 Booleans - Indicates in which days the task needs to be done with 1, from Sunday to Saturday
+        //e.g. [0, 0, 0, 1, 0, 1, 0] indicates the task needs to be done on Wednesday and Friday
+        this.weekDays = weekDays;
+
+        //[Int, Int] - Beginning of which time the task was allocated in [hours, minutes]
+        this.timeBegin = timeBegin;
+        
+        //[Int, Int] - End of which time the task was allocated in [hours, minutes]
+        this.timeEnd = timeEnd;
     }
 
+    //Getters
     get getDescription(){
         return this.description;
     }
@@ -33,6 +57,19 @@ class Task{
         return this.isDone;
     }
 
+    get getWeekDays(){
+        return this.weekDays;
+    }
+
+    get getTimeBegin(){
+        return this.timeBegin;
+    }
+
+    get getTimeEnd(){
+        return this.timeEnd;
+    }
+
+    //Setters
     setDescription(_description){
         this.description = _description;
     }
@@ -51,5 +88,17 @@ class Task{
 
     setIsDone(_isDone){
         this.isDone = _isDone;
+    }
+
+    setWeekDays(_weekDays){
+        this.weekDays = _weekDays;
+    }
+
+    setTimeBegin(_timeBegin){
+        this.timeBegin = _timeBegin;
+    }
+
+    setTimeEnd(_timeEnd){
+        this.timeEnd = _timeEnd;
     }
 }
